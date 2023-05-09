8aa894756ff490fddbd139f1c69c87df2ff2e311
Muddassir Lateef <muddassirlateef001@gmail.com>
2023-05-09 13:06:46 +0500

    Imported Image as an array

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request
 import requests
 import ee
 import geehydro
-
+import cv2
+import urllib
+import numpy as np
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,13 +17,13 @@ def index():
         longitude = float(request.form['longitude'])
         scale = float(request.form['scale'])
 
-
         ee.Initialize()
 
-        poi = ee.Geometry.Point([longitude,latitude ]) # [long, lat]
+        poi = ee.Geometry.Point([longitude, latitude])  # [long, lat]
         roi = poi.buffer(distance=9.2e5)
 
-        dataset = ee.ImageCollection("COPERNICUS/Landcover/100m/Proba-V-C3/Global")
+        dataset = ee.ImageCollection(
+            "COPERNICUS/Landcover/100m/Proba-V-C3/Global")
         dataset = dataset.select('discrete_classification')
 
         date_filter = ee.Filter.date('2019-1-1', '2019-12-31')
@@ -28,7 +31,7 @@ def index():
 
         image = dataset.mean()
         image = image.multiply(scale)
-        visualisation_params = { 
+        visualisation_params = {
         'min': 0,
         'max': 126,
         'dimensions': 620,
@@ -38,12 +41,18 @@ def index():
         urla = image.getThumbUrl(visualisation_params)
         print(urla)
 
+        barry = ""
+
+        with urllib.request.urlopen(urla) as url:
+            barry = url.read()
+        arr = np.asarray(bytearray(barry), dtype=np.uint8)
+        img = cv2.imdecode(arr, -1)
 
         response = requests.get(urla)
 
         # Render the index template with the image URL
         return render_template('index.html', image_url=urla)
-    
+
     # Render the index template without an image URL
     return render_template('index.html', image_url=None)
 
